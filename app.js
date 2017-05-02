3d7b5e364ebf5c33ad8ce13496590e204ddfa920
rohjs <jiseung.roh@gmail.com>
2017-05-02 11:10:06 +0900

    add TimelineLite, refactor animation sequence

--- app.js
+++ app.js
@@ -3,28 +3,14 @@
       brand_text = document.querySelector('.brand + p'),
       demo_title = document.querySelector('.demo__title'),
       demo_list = document.querySelector('.demo__list'),
-      copyright = document.querySelector('.copyright');
-  TweenLite.from(brand, 1, {
-  	opacity: 0,
-  	y: -10
-  });
-  TweenLite.from(brand_text, 1, {
-  	opacity: 0,
-  	y: -10,
-  	delay: .5
-  });
-  TweenLite.from(demo_title, 1, {
-  	opacity: 0,
-  	y: -10,
-  	delay: 1
-  });
-  TweenLite.from(demo_list, 1, {
-  	opacity: 0,
-  	y: -10,
-  	delay: 1
-  });
-  TweenLite.from(copyright, 1.5, {
-  	opacity: 0,
-  	delay: 1.4
-  });
+      copyright = document.querySelector('.copyright'),
+      tl = new TimelineLite();
+
+  // TimelineLite
+  tl
+    .from(brand, 0.7, { opacity: 0, y: -10 })
+    .from(brand_text, 0.5, { opacity: 0, y: -10, }, '-=0.3')
+    .from(demo_title, 0.5, { opacity: 0, y: -10, }, '-=0.3')
+    .from(demo_list, 0.5, { opacity: 0, y: -10, }, '-=0.3')
+    .from(copyright, 1.5, { opacity: 0 }, '-=0.15');
 })(window);
